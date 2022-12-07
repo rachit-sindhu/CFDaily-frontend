291172d12691bcafcf29f6af90210f19165eff51
Sushant Mishra <70423224+sushantdev-git@users.noreply.github.com>
2022-12-07 17:21:40 +0530

    added demo video

--- src/components/LandingIntro/LandingIntro.jsx
+++ src/components/LandingIntro/LandingIntro.jsx
@@ -1,15 +1,23 @@
-import styles from './LandingIntro.module.css';
+import { Button } from "@mui/material";
+import styles from "./LandingIntro.module.css";
 
 const LandingIntro = () => {
-    return (
-        <div className={styles.LandingIntro}>
-            <div className={styles.Content}>
-                <h2>CF Daily</h2>
-
-                <h1 className={styles.TagLine}>Start your problem solving journey with us🚀</h1>
-            </div>
+  return (
+    <div className={styles.LandingIntro}>
+      <div className={styles.Content}>
+        <h2>CF Daily</h2>
+        {/* <video src='https://youtu.be/-6PZ8E0AcIs' width="80%" height="50%" controls autoPlay></video> */}
+        <div>
+            <h1 className={styles.TagLine}>
+                Start your problem solving journey with us🚀
+            </h1>
+            <a href="https://youtu.be/tphHCoBiQWE" target="_blank">
+            <Button  variant="contained">Demo</Button>
+            </a>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default LandingIntro;
